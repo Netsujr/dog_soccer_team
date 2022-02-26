855430d1ea1665d0ce233936d12970a8f3432ec2
Renato Delboni <renatonato_jr@hotmail.com>
2022-02-26 22:52:40 +0900

    fixed age/goals

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -2,24 +2,32 @@ import React, { createContext, useReducer } from "react";
 import AppReducer from "./AppReducer";
 import { getBreedsData } from '../api';
 const dogNames = require('dog-names');
-const age = Math.floor(Math.random() * 20) + 1;
-const goals = Math.floor(Math.random() * 20) + 1;
 const random = Math.floor(Math.random() * 80) + 1;
 
-getBreedsData().then(data => {
-  console.log(Object.keys(data)[random])});
+function randomAge() {
+  return (Math.floor(Math.random() * 20) + 1).toString();
+}
 
-  function randomAge() {
-    return Math.floor(Math.random() * 20) + 1;
-  }
+function randomGoals() {
+  return (Math.floor(Math.random() * 15) + 1).toString();
+}
+
+function breeds() {
+  getBreedsData().then(data => {
+    return console.log(Object.keys(data)[random]);
+  });
+};
+
+
+
 
 
 const initialState = {
   dogs: [
     { id: 1, name: `${dogNames.allRandom()}`, age: "", breed: "", goals: "", image: '' },
-    { id: 2, name: `${dogNames.allRandom()}`, age: randomAge(), breed: "Jack Russell", goals: goals, image: '' },
-    { id: 3, name: `${dogNames.allRandom()}`, age: age, breed: "Labrador", goals: goals, image: '' },
-    { id: 4, name: `${dogNames.allRandom()}`, age: age, breed: "Poodle", goals: goals, image: '' },
+    { id: 2, name: `${dogNames.allRandom()}`, age: randomAge(), breed: breeds(), goals: randomGoals(), image: '' },
+    { id: 3, name: `${dogNames.allRandom()}`, age: randomAge(), breed: "Labrador", goals: randomGoals(), image: '' },
+    { id: 4, name: `${dogNames.allRandom()}`, age: randomAge(), breed: "Poodle", goals: randomGoals(), image: '' },
   ]
 };
 
